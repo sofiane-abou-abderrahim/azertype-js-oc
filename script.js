610d5f9e26298f7624bb9a625fca51297e0f3d9e
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-12-02 23:03:47 +0100

    implemented algorithm for comparison between userWord and the first word of listOfWords array

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
-const applicationWord = 'Bonjour';
+const listOfWords = ['Cachalot', 'Pétunia', 'Serviette'];
+let score = 0;
 
-let userWord = prompt('Entrez le mot : ' + applicationWord);
+let userWord = prompt('Entrez le mot : ' + listOfWords[0]);
 
-// if (userWord === applicationWord) {
-//   console.log('Bravo !');
-// } else {
-//   console.log('Vous avez fait une erreur de frappe.');
-// }
-
-switch (userWord) {
-  case applicationWord:
-    console.log('Bravo !');
-    break;
-  case 'Gredin':
-    console.log('Restez correct !');
-    break;
-  case 'Mécréant':
-    console.log('Restez correct !');
-    break;
-  case 'Vilain':
-    console.log('Soyez gentil !');
-    break;
-  default:
-    console.log('Vous avez fait une erreur de frappe.');
+if (userWord === listOfWords[0]) {
+  console.log('Bravo !');
+} else {
+  console.log('Vous avez fait une erreur de frappe.');
 }
+
+// switch (userWord) {
+//   case applicationWord:
+//     console.log('Bravo !');
+//     break;
+//   case 'Gredin':
+//     console.log('Restez correct !');
+//     break;
+//   case 'Mécréant':
+//     console.log('Restez correct !');
+//     break;
+//   case 'Vilain':
+//     console.log('Soyez gentil !');
+//     break;
+//   default:
+//     console.log('Vous avez fait une erreur de frappe.');
+// }
