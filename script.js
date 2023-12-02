2a38cd0ebacc51c6d08c6d7c8decac8ba04a8773
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-12-03 00:51:06 +0100

    used while loop to chose between mots or phrases + for loop to ask to write each word or phrase

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
-const listOfWords = ['Cachalot', 'Pétunia', 'Serviette'];
-let score = 0;
-let userWord = prompt('Entrez le mot : ' + listOfWords[0]);
+const words = ['Cachalot', 'Pétunia', 'Serviette'];
+const phrases = [
+  'Pas de panique !',
+  'La vie est belle',
+  'Merci pour le cadeau'
+];
 
-for (let i = 0; i < listOfWords.length; i++) {
-  while (userWord !== listOfWords[i]) {
-    userWord = prompt('Entrez le mot : ' + listOfWords[i]);
+let userWord = prompt('mots ou phrases');
+let score = 0;
+
+while (userWord !== 'mots' && userWord !== 'phrases') {
+  userWord = prompt('mots ou phrases');
+}
+
+if (userWord === 'mots') {
+  for (let i = 0; i < words.length; i++) {
+    while (userWord !== words[i]) {
+      userWord = prompt('Entrez le mot : ' + words[i]);
+    }
+    score++;
+    console.log('Bravo ! Votre score est de: ' + score);
   }
-  score++;
-  console.log('Bravo ! Votre score est de: ' + score);
+} else if (userWord === 'phrases') {
+  for (let i = 0; i < phrases.length; i++) {
+    while (userWord !== phrases[i]) {
+      userWord = prompt('Entrez la phrase : ' + phrases[i]);
+    }
+    score++;
+    console.log('Bravo ! Votre score est de: ' + score);
+  }
 }
 
 console.log('SCORE FINAL : ' + score + '/3');
