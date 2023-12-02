f14a006f1fe506bf29e245f97ce38ef272eec37e
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-12-02 22:53:22 +0100

    implemented the algorithm comparison between userWord and applicationWord with switch/case conditional structure

--- script.js
+++ script.js
@@ -2,8 +2,25 @@ const applicationWord = 'Bonjour';
 
 let userWord = prompt('Entrez le mot : ' + applicationWord);
 
-if (userWord === applicationWord) {
-  console.log('Bravo !');
-} else {
-  console.log('Vous avez fait une erreur de frappe.');
+// if (userWord === applicationWord) {
+//   console.log('Bravo !');
+// } else {
+//   console.log('Vous avez fait une erreur de frappe.');
+// }
+
+switch (userWord) {
+  case applicationWord:
+    console.log('Bravo !');
+    break;
+  case 'Gredin':
+    console.log('Restez correct !');
+    break;
+  case 'Mécréant':
+    console.log('Restez correct !');
+    break;
+  case 'Vilain':
+    console.log('Soyez gentil !');
+    break;
+  default:
+    console.log('Vous avez fait une erreur de frappe.');
 }
