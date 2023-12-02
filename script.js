2c3e5fece841a09e13cf7b3796d630be78fdd29a
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-12-02 23:24:03 +0100

    Implemented comparison algorithm and incremented the score when comparison is true

--- script.js
+++ script.js
@@ -4,40 +4,28 @@ let score = 0;
 let userWord = prompt('Entrez le mot : ' + listOfWords[0]);
 
 if (userWord === listOfWords[0]) {
-  console.log('Bravo !');
+  score++;
+  console.log('Bravo ! Votre score est de: ' + score);
 } else {
-  console.log('Vous avez fait une erreur de frappe.');
+  console.log('Erreur ! Votre score est toujours de: ' + score);
 }
 
-let userWord2 = prompt('Entrez le mot : ' + listOfWords[1]);
+userWord2 = prompt('Entrez le mot : ' + listOfWords[1]);
 
 if (userWord2 === listOfWords[1]) {
-  console.log('Bravo !');
+  score++;
+  console.log('Bravo ! Votre score est de: ' + score);
 } else {
-  console.log('Vous avez fait une erreur de frappe.');
+  console.log('Erreur ! Votre score est toujours de: ' + score);
 }
 
-let userWord3 = prompt('Entrez le mot : ' + listOfWords[2]);
+userWord3 = prompt('Entrez le mot : ' + listOfWords[2]);
 
 if (userWord3 === listOfWords[2]) {
-  console.log('Bravo !');
+  score++;
+  console.log('Bravo ! Votre score est de: ' + score);
 } else {
-  console.log('Vous avez fait une erreur de frappe.');
+  console.log('Erreur ! Votre score est toujours de: ' + score);
 }
 
-// switch (userWord) {
-//   case applicationWord:
-//     console.log('Bravo !');
-//     break;
-//   case 'Gredin':
-//     console.log('Restez correct !');
-//     break;
-//   case 'Mécréant':
-//     console.log('Restez correct !');
-//     break;
-//   case 'Vilain':
-//     console.log('Soyez gentil !');
-//     break;
-//   default:
-//     console.log('Vous avez fait une erreur de frappe.');
-// }
+console.log('VOTRE SCORE FINAL : ' + score);
