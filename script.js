4ca25b9ec339d7b0ca0c1c55c7fc0f2aa71b51cc
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-12-02 23:07:12 +0100

    implemented algorithm for comparison between userWord and the second & third word of listOfWords array

--- script.js
+++ script.js
@@ -9,6 +9,22 @@ if (userWord === listOfWords[0]) {
   console.log('Vous avez fait une erreur de frappe.');
 }
 
+let userWord2 = prompt('Entrez le mot : ' + listOfWords[1]);
+
+if (userWord2 === listOfWords[1]) {
+  console.log('Bravo !');
+} else {
+  console.log('Vous avez fait une erreur de frappe.');
+}
+
+let userWord3 = prompt('Entrez le mot : ' + listOfWords[2]);
+
+if (userWord3 === listOfWords[2]) {
+  console.log('Bravo !');
+} else {
+  console.log('Vous avez fait une erreur de frappe.');
+}
+
 // switch (userWord) {
 //   case applicationWord:
 //     console.log('Bravo !');
