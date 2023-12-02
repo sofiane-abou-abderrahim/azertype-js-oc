44d420adae4a0f2a60faba14c74a268e0a0f7c4d
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-12-03 00:31:25 +0100

    used for & while loop to ask the user to type each word of the list and increment the score

--- script.js
+++ script.js
@@ -1,31 +1,13 @@
 const listOfWords = ['Cachalot', 'Pétunia', 'Serviette'];
 let score = 0;
-
 let userWord = prompt('Entrez le mot : ' + listOfWords[0]);
 
-if (userWord === listOfWords[0]) {
+for (let i = 0; i < listOfWords.length; i++) {
+  while (userWord !== listOfWords[i]) {
+    userWord = prompt('Entrez le mot : ' + listOfWords[i]);
+  }
   score++;
   console.log('Bravo ! Votre score est de: ' + score);
-} else {
-  console.log('Erreur ! Votre score est toujours de: ' + score);
 }
 
-userWord2 = prompt('Entrez le mot : ' + listOfWords[1]);
-
-if (userWord2 === listOfWords[1]) {
-  score++;
-  console.log('Bravo ! Votre score est de: ' + score);
-} else {
-  console.log('Erreur ! Votre score est toujours de: ' + score);
-}
-
-userWord3 = prompt('Entrez le mot : ' + listOfWords[2]);
-
-if (userWord3 === listOfWords[2]) {
-  score++;
-  console.log('Bravo ! Votre score est de: ' + score);
-} else {
-  console.log('Erreur ! Votre score est toujours de: ' + score);
-}
-
-console.log('VOTRE SCORE FINAL : ' + score + '/3');
+console.log('SCORE FINAL : ' + score + '/3');
