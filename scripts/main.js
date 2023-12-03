1d6af9eb85f5f1caafca354d76961cea9b6abdb5
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-12-03 17:04:21 +0100

    selected proposition area & score area elements with querySelector + source option list with querySelectorAll

--- scripts/main.js
+++ scripts/main.js
@@ -6,5 +6,14 @@
 
 const userTextInpuElement = document.getElementById('textInput');
 const validateWordBtnElement = document.getElementById('validateWordBtn');
+const propositionAreaElement = document.querySelector('.propositionArea');
+const scoreAreaElement = document.querySelector('.scoreArea');
+const sourceOptionList = document.querySelectorAll('.sourceOption');
 
-launchGame();
+console.log(userTextInpuElement);
+console.log(validateWordBtnElement);
+console.log(propositionAreaElement);
+console.log(scoreAreaElement);
+console.log(sourceOptionList);
+
+// launchGame();
