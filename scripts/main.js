3d5032453b0eb505429fae4fc64b41c942343ddf
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-12-03 22:52:30 +0100

    cleaned the code

--- scripts/main.js
+++ scripts/main.js
@@ -5,18 +5,9 @@
  *********************************************************************************/
 
 const userTextInpuElement = document.getElementById('textInput');
-console.log(userTextInpuElement);
-
 const validateWordBtnElement = document.getElementById('validateWordBtn');
-console.log(validateWordBtnElement);
-
 const propositionAreaElement = document.querySelector('.propositionArea');
-console.log(propositionAreaElement);
-
 const scoreAreaSpanElement = document.querySelector('.scoreArea span');
-console.log(scoreAreaSpanElement);
-
 const radioBtnList = document.querySelectorAll('.sourceOption input');
-console.log(radioBtnList);
 
 launchGame();
--- scripts/script.js
+++ scripts/script.js
@@ -3,46 +3,9 @@ function showResult(score, numberOfQuestions) {
   scoreAreaSpanElement.innerHTML = scoreDisplay;
 }
 
-function choseWordsOrPhrases() {
-  // Declaration of the variable containing the user's choice
-  let userChoice = prompt(
-    "Avec quelle liste désirez-vous jouer : 'mots' ou 'phrases' ?"
-  );
-
-  // While the user has not entered "mots" or "phrases," they are prompted to enter a userChoice again
-  while (userChoice !== 'mots' && userChoice !== 'phrases') {
-    userChoice = prompt(
-      "Avec quelle liste désirez-vous jouer : 'mots' ou 'phrases' ?"
-    );
-  }
-  return userChoice;
-}
-
-function launchGameLoop(propositionsList) {
-  let score = 0;
-  // We traverse the array of words
-  for (let i = 0; i < propositionsList.length; i++) {
-    // We ask the user to enter the word corresponding to index i
-    let userWord = prompt('Entrez le mot : ' + propositionsList[i]);
-    if (userWord === propositionsList[i]) {
-      // If the word entered by the user is correct, we increment the score
-      score++;
-    }
-  }
-  return score;
-}
-
 function launchGame() {
-  let userChoice = choseWordsOrPhrases();
-
   let score = 0;
   let proposedWordsNumber = 0;
-  if (userChoice === 'mots') {
-    score = launchGameLoop(wordsList);
-    proposedWordsNumber = wordsList.length;
-  } else {
-    score = launchGameLoop(phrasesList);
-    proposedWordsNumber = phrasesList.length;
-  }
+
   showResult(score, proposedWordsNumber);
 }
