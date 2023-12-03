5f434f81db5b27fffb98614334ab06e735a8e137
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-12-03 14:58:39 +0100

    created config.js file + inserted propositions lists inside

---
+++ config.js
@@ -0,0 +1,7 @@
+// Declaration of arrays containing lists of words suggested to the user
+const wordsList = ['Cachalot', 'Pétunia', 'Serviette'];
+const phrasesList = [
+  'Pas de panique !',
+  'La vie est belle',
+  'Merci pour le cadeau'
+];
--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-// Declaration of arrays containing lists of words suggested to the user
-const wordsList = ['Cachalot', 'Pétunia', 'Serviette'];
-const phrasesList = [
-  'Pas de panique !',
-  'La vie est belle',
-  'Merci pour le cadeau'
-];
-
 function showResult(score, numberOfQuestions) {
   console.log(
     'Votre score final est de ' + score + ' sur ' + numberOfQuestions
