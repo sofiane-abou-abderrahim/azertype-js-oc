2fd64776b783106ad17566371ff7b6ee5b08ffa6
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-12-04 11:50:56 +0100

    declared propositionList and set it up to wordsList as initial value + added if check on event target value

--- scripts/script.js
+++ scripts/script.js
@@ -10,28 +10,34 @@ function displayProposition(proposition) {
 function launchGame() {
   let score = 0;
   let i = 0;
+  let propositionList = wordsList;
 
-  displayProposition(wordsList[i]);
+  displayProposition(propositionList[i]);
 
   validateWordBtnElement.addEventListener('click', () => {
     console.log(userTextInpuElement.value);
-    if (userTextInpuElement.value === wordsList[i]) {
+    if (userTextInpuElement.value === propositionList[i]) {
       score++;
     }
     i++;
     showResult(score, i);
     userTextInpuElement.value = '';
-    if (wordsList[i] === undefined) {
+    if (propositionList[i] === undefined) {
       displayProposition('Le jeu est fini');
       validateWordBtnElement.disabled = true;
     } else {
-      displayProposition(wordsList[i]);
+      displayProposition(propositionList[i]);
     }
   });
 
   for (let j = 0; j < radioBtnList.length; j++) {
     radioBtnList[j].addEventListener('change', event => {
       console.log(event.target.value);
+      if (event.target.value === '1') {
+        propositionList = wordsList;
+      } else {
+        propositionList = phrasesList;
+      }
     });
   }
 
